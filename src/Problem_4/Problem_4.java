b731044d94f81232ed5520cb4540940ef0ca0458
Olga Zawislak <olga.zawislak99@gmail.com>
2020-03-22 21:28:35 +0100

    Problem 4

--- src/Problem_4/Problem_4.java
+++ src/Problem_4/Problem_4.java
@@ -1,4 +1,38 @@
 package Problem_4;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Problem_4 {
+
+    private int findMissing(ArrayList<Integer> lst){
+        int miss =1;
+        int tmp =0;
+        Integer max = findMax(lst);
+        miss =max-1;
+
+        for (Integer i : lst) {
+            if (i == miss) {
+                tmp = miss - 1;
+                break;
+            }
+        }
+        miss=tmp;
+        if(tmp <= 0){
+            miss=max+1;
+        }
+        return miss;
+    }
+
+    private Integer findMax(ArrayList<Integer> lst){
+        return Collections.max(lst);
+    }
+
+    public static void main(String[] args){
+        ArrayList<Integer> lst = new ArrayList<>();
+        lst.add(0);
+        lst.add(1);
+        lst.add(2);
+        System.out.println(new Problem_4().findMissing(lst));
+
+    }
 }
