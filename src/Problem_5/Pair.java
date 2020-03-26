8302f0035b3324df9aedc2b16e25d0efb32b49e7
Olga Zawislak <olga.zawislak99@gmail.com>
2020-03-26 10:26:38 +0100

    add Problem_5

---
+++ src/Problem_5/Pair.java
@@ -0,0 +1,28 @@
+package Problem_5;
+
+public class Pair {
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    private int b;
+
+    public Pair(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+}
---
+++ src/Problem_5/Problem_5.java
@@ -0,0 +1,25 @@
+package Problem_5;
+
+public class Problem_5 {
+
+    private static int car(Pair p_cons){
+        return p_cons.getA();
+    }
+
+    private static int cdr(Pair p_cons){
+        return p_cons.getB();
+    }
+
+    private static Pair cons(int p_a, int p_b){
+        return new Pair(p_a,p_b);
+    }
+
+    public static void main(String[] args){
+        Pair pair = cons(3,4);
+        int car = car(pair);
+        int cdr = cdr(pair);
+        System.out.println("Car of pair is: " + car + "\n");
+        System.out.println("Cdr of pair is: " + cdr + "\n");
+
+    }
+}
